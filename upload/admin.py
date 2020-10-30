b689949f39cbde142e5138fc74392382b9eb4181
Mikhail Katsupko <t63723@gmail.com>
2020-10-30 16:52:55 +0300

    fix upload

--- upload/admin.py
+++ upload/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MyFiles
+
+@admin.register(MyFiles)
+class MyFilesAdmin(admin.ModelAdmin):
+    pass
--- upload/views.py
+++ upload/views.py
@@ -9,5 +9,8 @@ def upload_doc(request):
         if form.is_valid():
             # Do something with our files or simply save them
             # if saved, our files would be located in media/ folder under the project's base folder
+
             form.save()
-    return render(request, 'templates/index.html', locals())
+
+            
+    return render(request, 'index.html', locals())
