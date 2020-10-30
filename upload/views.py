21247d2681ca9380d458a63a8d4ec8cd025ee6b1
andrgayday <andrey.gayday.423@gmail.com>
2020-10-30 16:52:06 +0700

    init commit

---
+++ upload/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .forms import UploadDocumentForm
+
+
+def upload_doc(request):
+    form = UploadDocumentForm()
+    if request.method == 'POST':
+        form = UploadDocumentForm(request.POST, request.FILES)  # Do not forget to add: request.FILES
+        if form.is_valid():
+            # Do something with our files or simply save them
+            # if saved, our files would be located in media/ folder under the project's base folder
+            form.save()
+    return render(request, 'templates/index.html', locals())
